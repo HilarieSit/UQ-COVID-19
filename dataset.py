0727ff07625f137e229ba8cb567f43df5a858bb8
Hilarie Sit <hs764@cornell.edu>
2020-05-04 15:22:56 -0400

    add datagen

--- dataset.py
+++ dataset.py
@@ -1,13 +1,18 @@
 import numpy as np
 import cv2
 import pandas as pd
-from sklearn.preprocessing import OneHotEncoder
 import os
+from keras.utils import to_categorical
 
-def load_data(type, enc):
+# define class encoding
+encoding = {'NORMAL': 0, 'PNEUMONIA': 1}
+
+def load_data(type):
     try:
+        # load from npz
         X, y = np.load('data/'+type+'.npz')
     except:
+        # else load images
         images, labels = [], []
         for root, _, files in os.walk('data/'+type):
             for name in files:
@@ -16,23 +21,32 @@ def load_data(type, enc):
                     resize_img = cv2.resize(img, (224, 224))
                     images.append(resize_img)
                     labels.append(os.path.basename(root))
-                    break
         images = np.array(images)
-        labels = np.array(labels).reshape(1,-1)
-        if type == 'train':
-            y = enc.fit_transform(labels).toarray()
-        else:
-            y = enc.transform(labels).toarray()
-        np.savez('data/'+type+'.npz', X=images, y=y)
+        labels = np.array(labels)
+        # map to class encoding
+        u, ind = np.unique(labels, return_inverse=True)
+        y = np.array([encoding[x] for x in u])[ind].reshape(1,-1)
+        # map to one hot encoding
+        one_hot_y = to_categorical(ind)
+        # save to npz and load
+        np.savez('data/'+type+'.npz', X=images, y=one_hot_y)
         X, y = np.load('data/'+type+'.npz')
     return X, y
 
-def return_Xy():
-    X, y = dict(), dict()
-    enc = OneHotEncoder()
-    for type in ('train', 'val', 'test'):
-        X[type], y[type] = load_data(type, enc)
-    return X, y
+def get_datadict():
+    # return all splits in a dict
+    for type in ('train', 'test'):
+        data['X_'+type], data['y_'+type] = load_data(type)
+    return data
 
-X, y = return_Xy()
-print(X['train'])
+def get_datagen():
+    # code from Keras Image Preprocessing documentation
+    # https://keras.io/preprocessing/image/
+    datagen = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization=True,
+        rotation_range=20,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        horizontal_flip=True)
+    return datagen
--- mcdropout.py
+++ mcdropout.py
@@ -1,48 +1,55 @@
 import keras
 from keras import models
 from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+import numpy as np
+import dataset
+import seaborn as sns
 
-NUM_CLASSES = 2
-NUM_MC = 100
+NUM_CLASSES = 2         # number of classes
+NUM_MC = 100            # number of monte carlo samples
 
 class MCDropout:
-    def __init__(self, filepath, data=None):
+    def __init__(self, filepath, datagen=None, data=None):
         try:
             self.model = models.load_model(filepath+'/model.h5')
         except:
             self.data = data
-            self.model = self.train()
+            self.model = self.train(datagen)
 
-    def train(self):
-        inp = Input(shape)
-        x = Conv2D(16, kernel_size=(3, 3), activation=act)(inp)
+    def train(self, datagen):
+        # architecture
+        input = Input(shape=(244,244,3))
+        # default is channels last
+        x = Conv2D(16, kernel_size=(3, 3), activation=act)(input)
         x = MaxPooling2D(pool_size=(2, 2))(x)
+        x = Dropout(0.5)(x, training=True)
         x = Conv2D(32, kernel_size=(3, 3), activation=act)(x)
         x = MaxPooling2D(pool_size=(2, 2))(x)
-        x = Dropout(x, p=0.5, training=True)
+        x = Dropout(0.5)(x, training=True)
         x = Flatten()(x)
         x = Dense(128, activation='relu')(x)
-        x = Dropout(x, p=0.5)
-        out = Dense(NUM_CLASSES, activation='softmax')(x)
+        x = Dropout(0.5)(x)
+        output = Dense(NUM_CLASSES, activation='softmax')(x)
+        model = Model(inputs=input, outputs=output)
 
-        model = Model(inputs=inp, outputs=out)
+        # optimizer/loss
         optimizer = keras.optimizers.Adam(lr=1e-3 beta_1=0.9, beta_2=0.999,
             amsgrad=False)
-
         model.compile(loss=keras.losses.binary_crossentropy,
             optimizer=optimizer, metrics=['accuracy'])
 
-        checkpoint = callbacks.ModelCheckpoint('/model.h5',
-            monitor='acc', mode='val_acc', verbose=2,
-            save_best_only=True)
-
-        model.fit(self.data['X_train'], self.data['y_train'],
-            validation_data=[self.data['X_val'], self.data['y_val']],
-            epochs=50, batch_size=64, verbose=2,
-            callbacks=[checkpoint, csv_logger])
-
+        # train
+        model.fit_generator(datagen.flow(self.data['X_train'], self.data['y_train'], batch_size=64),
+            steps_per_epoch=len(x_train) / 32, epochs=50, verbose=2,
+            callbacks=[checkpoint])
         return model
 
-        def evaluate(self):
-            for i in range(NUM_MC):
-                model.predict(self.data['X_test'])
+    def evaluate(self):
+        pred = []
+        for i in range(NUM_MC):
+            # get Monte Carlo samples by sampling network NUM_MC times
+            pred.append(self.model.predict(self.data['X_test'])
+        meanpred = pred.mean()
+        stdpred = pred.std()
+
+MCDropout('models/', datagen=dataset.get_datagen(), data=dataset.get_datadict())
