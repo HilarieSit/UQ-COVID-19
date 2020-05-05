23bef286237a08f8c37edcbdcd7db3ea671af596
Hilarie Sit <hs764@cornell.edu>
2020-05-05 01:24:14 -0400

    fit datagen, fix checkpoint callback

--- mcdropout.py
+++ mcdropout.py
@@ -1,9 +1,17 @@
 import keras
-from keras import models
-from keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras.models import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from keras import optimizers
+from keras import callbacks
 import numpy as np
 import dataset
-import seaborn as sns
+
+# check if using gpu
+import tensorflow as tf
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
+from keras import backend as K
+K.tensorflow_backend._get_available_gpus()
+# import seaborn as sns
 
 NUM_CLASSES = 2         # number of classes
 NUM_MC = 100            # number of monte carlo samples
@@ -14,16 +22,17 @@ class MCDropout:
             self.model = models.load_model(filepath+'/model.h5')
         except:
             self.data = data
+            self.filepath = filepath
             self.model = self.train(datagen)
 
     def train(self, datagen):
         # architecture
-        input = Input(shape=(244,244,3))
+        input = Input(shape=(224, 224 ,3))
         # default is channels last
-        x = Conv2D(16, kernel_size=(3, 3), activation=act)(input)
+        x = Conv2D(16, kernel_size=(3, 3), activation='relu')(input)
         x = MaxPooling2D(pool_size=(2, 2))(x)
         x = Dropout(0.5)(x, training=True)
-        x = Conv2D(32, kernel_size=(3, 3), activation=act)(x)
+        x = Conv2D(32, kernel_size=(3, 3), activation='relu')(x)
         x = MaxPooling2D(pool_size=(2, 2))(x)
         x = Dropout(0.5)(x, training=True)
         x = Flatten()(x)
@@ -33,14 +42,20 @@ class MCDropout:
         model = Model(inputs=input, outputs=output)
 
         # optimizer/loss
-        optimizer = keras.optimizers.Adam(lr=1e-3 beta_1=0.9, beta_2=0.999,
+        optimizer = keras.optimizers.Adam(lr=1e-3, beta_1=0.9, beta_2=0.999,
             amsgrad=False)
         model.compile(loss=keras.losses.binary_crossentropy,
             optimizer=optimizer, metrics=['accuracy'])
+        checkpoint = callbacks.ModelCheckpoint(self.filepath+'/model.h5',
+            monitor='val_accuracy', mode='max', verbose=2, save_best_only=True)
 
         # train
-        model.fit_generator(datagen.flow(self.data['X_train'], self.data['y_train'], batch_size=64),
-            steps_per_epoch=len(x_train) / 32, epochs=50, verbose=2,
+        X_train = self.data['X_train']
+        y_train = self.data['y_train']
+
+        datagen.fit(X_train)
+        model.fit_generator(datagen.flow(X_train, y_train, batch_size=64),
+            steps_per_epoch=len(X_train) / 32, epochs=50, verbose=2,
             callbacks=[checkpoint])
         return model
 
@@ -48,8 +63,12 @@ class MCDropout:
         pred = []
         for i in range(NUM_MC):
             # get Monte Carlo samples by sampling network NUM_MC times
-            pred.append(self.model.predict(self.data['X_test'])
+            pred.append(self.model.predict(self.data['X_test']))
         meanpred = pred.mean()
         stdpred = pred.std()
 
-MCDropout('models/', datagen=dataset.get_datagen(), data=dataset.get_datadict())
+data = dataset.get_datadict()
+print(data['X_train'])
+datagen = dataset.get_datagen()
+
+MCDropout('models/', datagen=datagen, data=data)
